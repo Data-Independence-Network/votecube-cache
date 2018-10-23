3529d98175d0bb878feec18c0d2c344f6c746fa3
russoturisto <russoturisto@gmail.com>
2018-10-22 21:27:34 -0600

    compiling

--- server/src/cache/server.rs
+++ server/src/cache/server.rs
@@ -6,7 +6,6 @@ use net2::unix::UnixTcpBuilderExt;
 use num_cpus;
 use std::io;
 use std::net::ToSocketAddrs;
-use std::sync::Arc;
 use tokio;
 use std::thread;
 use tokio::net::{TcpStream, TcpListener};
@@ -20,26 +19,53 @@ use super::super::request::Request;
 use super::super::codes;
 use super::super::response::Response;
 
+pub static mut runner: Runner = Runner::new();
 
 pub trait App {
 
     fn get_response(
         &self,
-        path: str,
+        path: &str,
         request_body: &[u8],
     ) -> Vec<u8>;
 
 }
 
+pub struct DefaultApp {
+}
+
+impl DefaultApp {
+    fn new() -> DefaultApp {
+        DefaultApp {}
+    }
+}
+
+impl App for DefaultApp {
+
+    fn get_response(
+        &self,
+        path: &str,
+        request_body: &[u8],
+    ) -> Vec<u8> {
+        Vec::new()
+    }
+}
+
 pub struct Runner {
-    app: &'static App,
+    pub app: &'static mut App,
 }
 
 impl Runner {
 
+    fn new() -> Runner {
+        Runner {
+            app: &mut DefaultApp::new()
+        }
+    }
+
     /// Resolves a request, returning a future that is processable into a Response
 
-    pub fn resolve(&self, mut request: Request) -> impl Future<Item=Response, Error=io::Error> + Send {
+    fn resolve(&self, mut request: Request) -> impl Future<Item=Response, Error=io::Error> + Send {
         let response = self.get_response(&request);
 //        request.set_params(matched_route.params);
 
@@ -62,27 +88,25 @@ impl Runner {
             return response;
         }
 
-        let path = request.path().as_ref();
+        let path = request.path();
         let request_body = request.raw_body();
 
-        let data = self.app.get_response(*path, request_body);
+        let data = self.app.get_response(path, request_body);
 
         response.body_vec(data);
 
         response
     }
+
 }
 
 pub struct Server {
-
-    app: &'static App,
 }
 
 impl Server {
 
-    pub fn new (app: &'static App) -> Server {
+    pub fn new () -> Server {
         Server {
-            app
         }
     }
 
@@ -97,14 +121,14 @@ impl Server {
     ///
     /// https://users.rust-lang.org/t/getting-tokio-to-match-actix-web-performance/18659/7
     ///
-    pub fn start_small_load_optimized(mut self, mut runner: Runner, host: &str, port: u16) {
+    pub fn start_small_load_optimized(mut self, host: &str, port: u16) {
         let addr = (host, port).to_socket_addrs().unwrap().next().unwrap();
         let mut threads = Vec::new();
 //        app._route_parser.optimize();
-       let arc_runner = Arc::new(runner);
+    //    let arc_runner = Arc::new(*self.runner);
 
         for _ in 0..num_cpus::get() {
-            let arc_runner = arc_runner.clone();
+            // let arc_runner = arc_runner.clone();
 //            let arc_app = arc_app.clone();
             threads.push(thread::spawn(move || {
                 let mut runtime = tokio::runtime::current_thread::Runtime::new().unwrap();
@@ -122,9 +146,8 @@ impl Server {
                     let listener = TcpListener::from_std(listener, &tokio::reactor::Handle::current()).unwrap();
 
                     listener.incoming().for_each(move |socket| {
-                        process(runner, socket);
+                        process(socket);
                             Ok(())
-                        })).then(|_| future::ok(()));
                     })
                     .map_err(|err| eprintln!("accept error = {:?}", err))
                 });
@@ -140,52 +163,17 @@ impl Server {
             thread.join().unwrap();
         }
 
-       fn process(arc_runner: Arc<Runner>, socket: TcpStream) {
+       fn process(socket: TcpStream) {
            let framed = Framed::new(socket, Http);
            let (tx, rx) = framed.split();
 
            let task = tx.send_all(rx.and_then(move |request: Request| {
-               arc_runner.resolve(request)
+               runner.resolve(request)
            })).then(|_| future::ok(()));
 
            // Spawn the task that handles the connection.
            tokio::spawn(task);
        }
-    }
-
-    #[inline]
-    fn get_response(&self, request: &Request) -> Response {
-        let mut response = Response::new();
-
-        if request.method() != "PUT" {
-            response.body_vec(codes::INVALID_DATA_FORMAT_RESPONSE);
-            return response;
-        }
-
-        let path = request.path().as_ref();
-        let request_body = request.raw_body();
-
-        let data = self.app.get_response(*path, request_body);
-
-        response.body_vec(data);
-
-        response
-    }
-
-    /// Resolves a request, returning a future that is processable into a Response
-
-    fn resolve(&self, mut request: Request) -> impl Future<Item=Response, Error=io::Error> + Send {
-        let response = self.get_response(&request);
-//        request.set_params(matched_route.params);
-
-//        let context = (self.context_generator)(request);
-//        let return_value = Box::new(future::ok(context));
-
-//        return_value
-//            .and_then(|context| {
-//
-        future::ok(response)
-//            })
     }
 
 }
